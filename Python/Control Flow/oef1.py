b228943df7fc2975a9b3079909ed6143079df589
Thimen-Labeeuw <Thimen.Labeeuw@student.tsaam.be>
2025-09-03 10:15:24 +0200

    v2

---
+++ Python/Control Flow/oef1.py
@@ -0,0 +1,8 @@
+getal1 = int(input("geef een getal op"))
+getal2 = int(input("geef een getal op"))
+
+print(f"is waarde 1 groter dan waarde 2?: {getal1 > getal2}")
+print(f"is waarde 1 kleiner dan waarde 2?: {getal1 < getal2}")
+print(f"is waarde 1 gelijk aan waarde 2?: {getal1 == getal2}")
+print(f"is waarde 1 groter dan waarde 2? EN groter dan 10: {getal1 > getal2 and getal1 > 10}")
+print(f"is waarde 2 groter dan 5? EN waarde1 groter dan 10: {getal2 > 5 and getal1 > 10}")
---
+++ Python/Oefeningen/oef6.py
@@ -0,0 +1,5 @@
+getal1 = int(input("geef het eerste getal op"))
+getal2 = int(input("geef het tweede getal op"))
+
+print(getal1 + getal2)
+print(f"de som van {getal1} en {getal2} = {getal1 + getal2}")
